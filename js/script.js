d350d221556a86a2759f003ab20ac54dd59fdcfe
DesireeBesnard <desiree.besnard@gmail.com>
2021-03-16 09:35:43 +0100

    overlay added on responsive navbar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function (){
     if (btnHamburger.classList.contains('open')) {
         btnHamburger.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     } else {
         btnHamburger.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 });
